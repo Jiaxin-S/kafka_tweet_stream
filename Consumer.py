9c03459f0894d7532f441a4fb11140aa069d11be
Jiaxin Su <js4722@columbia.edu>
2016-11-04 17:27:14 -0400

    formatted consumer.py

--- Consumer.py
+++ Consumer.py
@@ -1,9 +1,13 @@
 from kafka import KafkaConsumer
 from kafka.errors import KafkaError
 
+def main():
+    consumer = KafkaConsumer(bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
+    consumer.subscribe(['test'])
 
-consumer = KafkaConsumer(bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
-consumer.subscribe(['test'])
+    for message in consumer:
+        print (message.value)
 
-for message in consumer:
-    print (message.value)
+
+if __name__ == '__main__':
+    main()
